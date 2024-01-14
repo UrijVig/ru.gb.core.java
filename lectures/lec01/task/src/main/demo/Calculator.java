3f0f8514c1f361ee2aefc4a656362a7f78ddd712
Gelvih Urij <gelvihurij@gmail.com>
2024-01-15 02:51:25 +0700

    Created a task for the first lecture

---
+++ lectures/lec01/task/src/main/demo/Calculator.java
@@ -0,0 +1,11 @@
+package src.main.demo;
+
+/**
+ * Класс для запуска приложения (точка входа)
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        UserView userView = new UserView();
+        userView.run();
+    }
+}
