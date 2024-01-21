5ed6009c63e82bf611e548a369b83459d4956ed2
Yuriy Gelvih <Gelvihurij@gmail.com>
2024-01-22 04:55:30 +0700

    checkWin method reworked

--- tictactoe/src/main/java/app/view/Field.java
+++ tictactoe/src/main/java/app/view/Field.java
@@ -54,46 +54,55 @@ public class Field {
 
     public void setDot(int x, int y, char dot) { // set dot and check fill and win
         map[x][y] = dot;
-        if (checkWin(HUMAN_DOT))
-            gameOverMsg = MSG_HUMAN_WON;
-        else if (checkWin(AI_DOT))
-            gameOverMsg = MSG_AI_WON;
-        else if (isMapFull())
-            gameOverMsg = MSG_DRAW;
+        if (checkWin(HUMAN_DOT)) gameOverMsg = MSG_HUMAN_WON;
+        else if (checkWin(AI_DOT)) gameOverMsg = MSG_AI_WON;
+        else if (isMapFull()) gameOverMsg = MSG_DRAW;
     }
 
     boolean isMapFull() {
         for (int i = 0; i < FIELD_SIZE; i++)
             for (int j = 0; j < FIELD_SIZE; j++)
-                if (map[i][j] == EMPTY_DOT)
-                    return false;
+                if (map[i][j] == EMPTY_DOT) return false;
         return true;
     }
 
+
     boolean checkWin(char dot) {
-        // checking horizontals / verticals
-        for (int i = 0; i < FIELD_SIZE; i++)
-            if ((map[i][0] == dot && map[i][1] == dot && map[i][2] == dot) ||
-                    (map[0][i] == dot && map[1][i] == dot && map[2][i] == dot))
-                return true;
-        // checking diagonals
-        if ((map[0][0] == dot && map[1][1] == dot && map[2][2] == dot) ||
-                (map[2][0] == dot && map[1][1] == dot && map[0][2] == dot))
-            return true;
-        return false;
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            boolean isHorizontalWin = true;
+            boolean isVerticalWin = true;
+            for (int j = 0; j < FIELD_SIZE; j++) {
+                if (map[i][j] != dot) isHorizontalWin = false;
+                if (map[j][i] != dot) isVerticalWin = false;
+            }
+            if (isHorizontalWin || isVerticalWin) return true;
+        }
+        boolean isDiagonal1Win = true;
+        boolean isDiagonal2Win = true;
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            if (map[i][i] != dot) isDiagonal1Win = false;
+            if (map[i][FIELD_SIZE - 1 - i] != dot) isDiagonal2Win = false;
+        }
+        return isDiagonal1Win || isDiagonal2Win;
     }
+//    boolean checkWin(char dot) {
+//        // checking horizontals / verticals
+//        for (int i = 0; i < FIELD_SIZE; i++)
+//            if ((map[i][0] == dot && map[i][1] == dot && map[i][2] == dot) ||
+//                    (map[0][i] == dot && map[1][i] == dot && map[2][i] == dot))
+//                return true;
+//        // checking diagonals
+//        return (map[0][0] == dot && map[1][1] == dot && map[2][2] == dot) ||
+//                (map[2][0] == dot && map[1][1] == dot && map[0][2] == dot);
+//    }
 
     public boolean isCellValid(int x, int y) {
-        if (x < 0 || y < 0 || x > FIELD_SIZE - 1 || y > FIELD_SIZE - 1)
-            return false;
-        if (map[x][y] == EMPTY_DOT)
-            return true;
-        return false;
+        if (x < 0 || y < 0 || x > FIELD_SIZE - 1 || y > FIELD_SIZE - 1) return false;
+        return map[x][y] == EMPTY_DOT;
     }
 
     public void paint(Graphics2D g) {
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         g.setColor(Color.lightGray);
         for (int i = 1; i < FIELD_SIZE; i++) {
@@ -107,16 +116,12 @@ public class Field {
             for (int x = 0; x < FIELD_SIZE; x++) {
                 if (map[x][y] == HUMAN_DOT) {
                     g.setColor(Color.blue);
-                    g.drawLine(x * CELL_SIZE + CELL_SIZE / 4, y * CELL_SIZE + CELL_SIZE / 4,
-                            (x + 1) * CELL_SIZE - CELL_SIZE / 4, (y + 1) * CELL_SIZE - CELL_SIZE / 4);
-                    g.drawLine(x * CELL_SIZE + CELL_SIZE / 4, (y + 1) * CELL_SIZE - CELL_SIZE / 4,
-                            (x + 1) * CELL_SIZE - CELL_SIZE / 4, y * CELL_SIZE + CELL_SIZE / 4);
+                    g.drawLine(x * CELL_SIZE + CELL_SIZE / 4, y * CELL_SIZE + CELL_SIZE / 4, (x + 1) * CELL_SIZE - CELL_SIZE / 4, (y + 1) * CELL_SIZE - CELL_SIZE / 4);
+                    g.drawLine(x * CELL_SIZE + CELL_SIZE / 4, (y + 1) * CELL_SIZE - CELL_SIZE / 4, (x + 1) * CELL_SIZE - CELL_SIZE / 4, y * CELL_SIZE + CELL_SIZE / 4);
                 }
                 if (map[x][y] == AI_DOT) {
                     g.setColor(Color.red);
-                    g.drawOval(x * CELL_SIZE + CELL_SIZE / 4, y * CELL_SIZE + CELL_SIZE / 4,
-                            CELL_SIZE / 2,
-                            CELL_SIZE / 2);
+                    g.drawOval(x * CELL_SIZE + CELL_SIZE / 4, y * CELL_SIZE + CELL_SIZE / 4, CELL_SIZE / 2, CELL_SIZE / 2);
                 }
             }
         }
